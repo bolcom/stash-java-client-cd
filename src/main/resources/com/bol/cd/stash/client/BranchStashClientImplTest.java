9e9de8ca530e3675a126b4b7799e743202416f57
Chris Kramer <ckramer@bol.com>
2014-04-14 15:58:44 +0200

    - Downgraded to jmockit 1.6 due to typecastingbug - Got the first working test

--- src/main/resources/com/bol/cd/stash/client/BranchStashClientImplTest.java
+++ src/main/resources/com/bol/cd/stash/client/BranchStashClientImplTest.java
@@ -1,5 +1,8 @@
 package com.bol.cd.stash.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Optional;
 
 import mockit.Expectations;
@@ -13,6 +16,8 @@ import com.atlassian.stash.rest.client.api.entity.Branch;
 import com.atlassian.stash.rest.client.core.http.HttpExecutor;
 import com.atlassian.stash.rest.client.core.http.HttpRequest;
 import com.atlassian.stash.rest.client.core.http.HttpResponseProcessor;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class BranchStashClientImplTest {
 
@@ -24,23 +29,47 @@ public class BranchStashClientImplTest {
 
     private ExtendedStashClient extendedStashClient;
 
+    private JsonParser jsonParser;
+
     @Before
     public void before() {
+        jsonParser = new JsonParser();
         extendedStashClient = new ExtendedStashClientImpl(httpExecutor);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
-    public void testCreateBranchFromMaster() {
+    public void testGetRepositoryBranch() {
 
+        
+        
         new Expectations() {
+        
             {
+                JsonElement jsonElement = getJsonTestObject("/com/bol/cd/stash/client/branches.json");
                 // @formatter:off
-                httpExecutor.execute((HttpRequest) any, (HttpResponseProcessor) any); result = null;
+                httpExecutor.execute((HttpRequest) any, (HttpResponseProcessor<JsonElement>) any); result = jsonElement;
                 // @formatter:on
             }
         };
         Optional<Branch> branch = extendedStashClient.getBranchStashClient().getRepositoryBranch(projectKey, repositorySlug, "ABC-123");
         Assert.assertTrue(branch.isPresent());
+        Assert.assertEquals("ABC-123", branch.get().getDisplayId());
+
+    }
+
+    private JsonElement getJsonTestObject(String name) {
+
+        
+        
+        JsonElement result = null;
+        try (InputStream is = this.getClass().getResourceAsStream(name); InputStreamReader isr = new InputStreamReader(is);) {
+
+            result = jsonParser.parse(isr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
 
     }
 }
