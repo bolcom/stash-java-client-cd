bb7ef581f9c18ad82da86016f7b48450c01d4638
Chris Kramer <ckramer@bol.com>
2014-04-15 13:59:22 +0200

    Refactor in progress - adding methods, working packages

--- src/main/java/com/bol/cd/stash/StashClient.java
+++ src/main/java/com/bol/cd/stash/StashClient.java
@@ -10,29 +10,20 @@ import feign.jaxrs.JAXRSModule;
 import java.util.Arrays;
 
 import com.bol.cd.stash.model.JsonApplicationMediaTypeInterceptor;
+import com.bol.cd.stash.request.DeleteBranch;
 
 public class StashClient {
 
     private static String projectKey = "TPT";
     private static String repositorySlug = "testrepository";
-    
+
     public static StashApi createClient() {
-        return Feign.builder()
-                .contract(new JAXRSModule.JAXRSContract())
-                .decoder(new JacksonDecoder())
-                .encoder(new JacksonEncoder())
-                .requestInterceptors(getRequestInterceptors())
-                .target(StashApi.class, "http://localhost:7990");
+        return Feign.builder().contract(new JAXRSModule.JAXRSContract()).decoder(new JacksonDecoder()).encoder(new JacksonEncoder())
+                .requestInterceptors(getRequestInterceptors()).target(StashApi.class, "http://localhost:7990");
     }
 
     private static Iterable<RequestInterceptor> getRequestInterceptors() {
-        return Arrays.asList(
-                new BasicAuthRequestInterceptor("admin", "password"),
-                new JsonApplicationMediaTypeInterceptor()
-        );
+        return Arrays.asList(new BasicAuthRequestInterceptor("admin", "password"), new JsonApplicationMediaTypeInterceptor());
     }
-    
-    public static void main(String[] args) {
-        System.out.println(createClient().getRepositoryBranches(projectKey, repositorySlug));
-    }
+
 }
---
+++ src/main/java/com/bol/cd/stash/request/CreateBranch.java
@@ -0,0 +1,34 @@
+package com.bol.cd.stash.request;
+
+/**
+ * <pre>
+ * {
+ *     "name": "feature/my-feature-branch",
+ *     "startPoint": "refs/heads/master"
+ * }
+ * </pre>
+ * 
+ * @author ckramer
+ */
+public class CreateBranch {
+
+    private String name;
+    private String startPoint;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+}
