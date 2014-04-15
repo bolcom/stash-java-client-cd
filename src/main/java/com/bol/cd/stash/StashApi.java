6483473905688ede1668b434e4c26dc762791b61
Chris Kramer <ckramer@bol.com>
2014-04-15 15:21:30 +0200

    - Now build sources and javadoc as well - Added create branch to stashApi

--- src/main/java/com/bol/cd/stash/StashApi.java
+++ src/main/java/com/bol/cd/stash/StashApi.java
@@ -1,16 +1,24 @@
 package com.bol.cd.stash;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
 import com.bol.cd.stash.model.Branch;
 import com.bol.cd.stash.model.Page;
+import com.bol.cd.stash.request.CreateBranch;
 import com.bol.cd.stash.request.DeleteBranch;
-
-import javax.ws.rs.*;
 
 /**
  * https://developer.atlassian.com/static/rest/stash/2.12.1/stash-branch-utils-rest.html
  * https://developer.atlassian.com/static/rest/stash/2.12.1/stash-rest.html
  */
+//@formatter:off
 public interface StashApi {
+
 
     @GET
     @Path("/rest/api/1.0/projects/{projectKey}/repos/{repositorySlug}/branches")
@@ -19,7 +27,7 @@ public interface StashApi {
             @PathParam("repositorySlug") String repositorySlug,
             @QueryParam("limit") int limit,
             @QueryParam("start") int start
-    );
+            );
 
     @GET
     @Path("/rest/api/1.0/projects/{projectKey}/repos/{repositorySlug}/branches")
@@ -32,21 +40,22 @@ public interface StashApi {
             @QueryParam("orderBy") String orderBy,
             @QueryParam("limit") int limit,
             @QueryParam("start") int start
-    );
+            );
 
     @GET
     @Path("/rest/api/1.0/projects/{projectKey}/repos/{repositorySlug}/branches/default")
     public Branch getRepositoryDefaultBranch(
             @PathParam("projectKey") String projectKey,
             @PathParam("repositorySlug") String repositorySlug
-    );
+            );
 
     @POST
     @Path("/rest/branch-utils/1.0/projects/{projectKey}/repos/{repositorySlug}/branches")
     public Branch createBranch(
             @PathParam("projectKey") String projectKey,
-            @PathParam("repositorySlug") String repositorySlug
-    );
+            @PathParam("repositorySlug") String repositorySlug,
+            CreateBranch createBranch
+            );
 
     @DELETE
     @Path("/rest/branch-utils/1.0/projects/{projectKey}/repos/{repositorySlug}/branches")
@@ -54,6 +63,7 @@ public interface StashApi {
             @PathParam("projectKey") String projectKey,
             @PathParam("repositorySlug") String repositorySlug,
             DeleteBranch deleteBranch
-    );
+            );
 
 }
+//@formatter:on
