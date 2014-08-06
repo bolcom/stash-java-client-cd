925274f63b897a16a298c71e45a4584f4203db0a
Jeroen Wijdemans <jwijdemans@bol.com>
2014-08-06 11:30:59 +0200

    DPI-2487 adding changeset to Ref

--- src/main/java/com/bol/cd/stash/model/Ref.java
+++ src/main/java/com/bol/cd/stash/model/Ref.java
@@ -2,6 +2,7 @@ package com.bol.cd.stash.model;
 
 public class Ref {
     private String id;
+    private String latestChangeset;
     private Repository repository;
 
     public String getId() {
@@ -10,6 +11,14 @@ public class Ref {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getLatestChangeset() {
+        return latestChangeset;
+    }
+
+    public void setLatestChangeset(String latestChangeset) {
+        this.latestChangeset = latestChangeset;
     }
 
     public Repository getRepository() {
